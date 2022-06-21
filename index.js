7c600c0f603a47ffa1015f0174cdbbe44ac0f0ec
Sagetony <uchetony224@gmail.com>
2022-06-21 15:08:16 +0100

    withdraw and balance

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ import { abi, contractAddress } from "./constants.js"
 
 const connectButton = document.getElementById("connectButton")
 const fundButton = document.getElementById("fundButton")
+const balanceButton = document.getElementById("balanceButton")
+const withdrawButton = document.getElementById("withdrawButton")
+
 connectButton.onclick = connect
 fundButton.onclick = fund
+withdrawButton.onclick = withdraw
+balanceButton.onclick = balance
 
 async function connect() {
     if (typeof window.ethereum != "undefined") {
@@ -21,7 +26,7 @@ async function connect() {
 
 // fund
 async function fund() {
-    let ethAmount = "77"
+    let ethAmount = document.getElementById("amount").value
     if (typeof window.ethereum != "undefined") {
         console.log("funding..")
         const provider = new ethers.providers.Web3Provider(window.ethereum)
@@ -31,11 +36,48 @@ async function fund() {
             const transactionResponse = await contract.fund({
                 value: ethers.utils.parseEther(ethAmount),
             })
-            console.log(ethAmount)
+            await listenTransaction(transactionResponse, provider)
+            console.log("Done!!")
         } catch (error) {
             console.log(error)
         }
     } else {
         connectButton.innerHTML = "Please Install Metamask"
     }
+}
+// balance
+async function balance() {
+    if (typeof window.ethereum != "undefined") {
+        console.log("balance..")
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const balance = await provider.getBalance(contractAddress)
+        console.log(ethers.utils.formatEther(balance))
+    }
+}
+// withdraw
+async function withdraw() {
+    if (typeof window.ethereum != "undefined") {
+        console.log("withdraw..")
+
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const Contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await Contract.withdraw()
+            await listenTransaction(transactionResponse, provider)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+function listenTransaction(transactionResponse, provider) {
+    console.log("Getting Transaction Receipt")
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionResponse) => {
+            console.log(
+                `Completed with ${transactionResponse.confirmations} confirmations`
+            )
+            resolve()
+        })
+    })
 }
